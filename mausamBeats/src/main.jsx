b49452f7321955cdcdc9513399815337308ad8e1
LataSingh07 <latakumarisingh5574@gmail.com>
2025-06-13 00:13:09 +0530

    main

--- mausamBeats/src/main.jsx
+++ mausamBeats/src/main.jsx
@@ -1,15 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import Login from './pages/Login';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
